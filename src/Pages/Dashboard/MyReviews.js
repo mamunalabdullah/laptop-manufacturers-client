81cc93498c3a7dc627d32c872eb5a952b02cac3b
Abdullah Al Mamun <abdullahmamun1597@gmail.com>
2022-05-25 12:53:59 +0600

    my reviews add

--- src/Pages/Dashboard/MyReviews.js
+++ src/Pages/Dashboard/MyReviews.js
@@ -1,10 +1,66 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { toast } from 'react-toastify';
+import auth from "../../Firebase/firebase.init";
 
 const MyReviews = () => {
+    const [review, setReview] = useState([]);
+    const [user] = useAuthState(auth);
+    const handleReview = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const feedback = event.target.feedback.value;
+        const rating = event.target.rating.value;
+        const review = {name, email, feedback, rating};
+        const url = "http://localhost:5000/review";
+        
+        axios.post(url, review)
+    .then(res => {
+        console.log(res)
+        event.target.reset()
+        toast("Review Add")   
+    })
+    }
     return (
-        <div>
-            <h1>this is reviews page</h1>
+        <div class="hero min-h-screen bg-base-100">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <div class="card flex-shrink-0 w-96 shadow-2xl bg-base-200">
+      <div class="card-body">
+        <form onSubmit={handleReview}>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Name</span>
+          </label>
+          <input type="text" value={user?.displayName} name="name" class="input input-bordered" />
         </div>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Email</span>
+          </label>
+          <input type="email" value={user?.email} name="email" class="input input-bordered" />
+        </div>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Your Feedback</span>
+          </label>
+          <input type="text" placeholder='Enter your feedback' name="feedback" class="input input-bordered" />
+        </div>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Your Ratings</span>
+          </label>
+          <input type="number" placeholder='Enter your ratings' name="rating" class="input input-bordered" />
+        </div>
+        <div class="form-control mt-6">
+          <button class="btn btn-primary">Review</button>
+        </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
     );
 };
 
